7ee3b4666e13174da02ae5ef657d5b9f52dc5447
UmangGadahvana <48270688+umanggadhavana@users.noreply.github.com>
2019-07-29 12:10:56 +0530

    Add files via upload

--- ai.js
+++ ai.js
@@ -6,40 +6,45 @@ var tempName = 0;
 var arrCross = [];
 var arrCircle = [];
 var ply1=0,ply2=0;
-var tempAttributeName1 = [];
-var tempAttributeName2 = [];
-
-window.onload = userIndicate(0);
-
-function userIndicate(userIndicator) {
-	if (userIndicator == 0){
-		document.querySelector('#player_1_name').style.cssText = 'border-bottom-style: solid;';
-	} else {
-		document.querySelector('#player_2_name').style.cssText = 'border-bottom-style: solid;';
-	}
-}
 
 function addImg(move) {
-	if (flag == 0){
-		document.querySelector('#player_2_name').style.cssText = 'border-bottom-style: solid;';
-		document.querySelector('#player_1_name').style.cssText = 'border-bottom-style: none;';
-		tempAttributeName1.push(document.querySelector(`#${move}`).getAttribute('class'));
-		document.querySelector(`#${move}`).setAttribute('class', `${document.querySelector(`#${move}`).getAttribute('class')} ugAnimate`);
-		document.querySelector(`#${move}`).innerHTML = 'X';
-		document.querySelector(`#${move}`).removeAttribute('onclick');
-		arrCross.push(move);
-		flag = 1;
-	}
-	else {
-		document.querySelector('#player_1_name').style.cssText = 'border-bottom-style: solid;';
-		document.querySelector('#player_2_name').style.cssText = 'border-bottom-style: none;';
-		tempAttributeName2.push(document.querySelector(`#${move}`).getAttribute('class'));
-		document.querySelector(`#${move}`).setAttribute('class', `${document.querySelector(`#${move}`).getAttribute('class')} ugAnimate`);
-		document.querySelector(`#${move}`).innerHTML = 'O';
-		document.querySelector(`#${move}`).removeAttribute('onclick');
-		arrCircle.push(move);
-		flag = 0;
-	}
+
+	document.querySelector(`#${move}`).setAttribute('class', `${document.querySelector(`#${move}`).getAttribute('class')} ugAnimate`);
+	document.querySelector(`#${move}`).innerHTML = 'X';
+	document.querySelector(`#${move}`).removeAttribute('onclick');
+    arrCross.push(move);
+
+    var conditionCross = [
+        arrCross.includes('one') && arrCross.includes('two') && arrCross.includes('three'),
+        arrCross.includes('four') && arrCross.includes('five') && arrCross.includes('six'),
+        arrCross.includes('seven') && arrCross.includes('eight') && arrCross.includes('nine'),
+        arrCross.includes('one') && arrCross.includes('four') && arrCross.includes('seven'),
+        arrCross.includes('two') && arrCross.includes('five') && arrCross.includes('eight'),
+        arrCross.includes('three') && arrCross.includes('six') && arrCross.includes('nine'),
+        arrCross.includes('one') && arrCross.includes('five') && arrCross.includes('nine'),	
+        arrCross.includes('three') && arrCross.includes('five') && arrCross.includes('seven')];
+
+        for (var a=0; a<8; a++){
+            if (conditionCross[a] == true)
+            {
+                ply1++;
+                tempName = a;
+                resetImg('X');
+            }
+        }
+    
+        
+    for (let k=0; k<=8; k++) {
+         if (document.querySelector(`#${arrEliments[k]}`).getAttribute('onclick')) {
+            setTimeout(function(){  
+                document.querySelector(`#${arrEliments[k]}`).innerHTML = 'O';
+                document.querySelector(`#${arrEliments[k]}`).setAttribute('class', `${document.querySelector(`#${arrEliments[k]}`).getAttribute('class')} ugAnimate`);
+                document.querySelector(`#${arrEliments[k]}`).removeAttribute('onclick');
+            }, 100);
+            arrCircle.push(arrEliments[k]);
+            break;
+        }
+    }
 
 	var conditionCross = [
 	arrCross.includes('one') && arrCross.includes('two') && arrCross.includes('three'),
@@ -64,27 +69,16 @@ function addImg(move) {
 	for (var a=0; a<8; a++){
 		if (conditionCross[a] == true)
 		{
-			ply1++;
 			tempName = a;
-			flag = 0;
-			document.querySelector('#player_1_name').style.cssText = 'border-bottom-style: none;';
-			document.querySelector('#player_2_name').style.cssText = 'border-bottom-style: none;';
-			userIndicate(0);
-			resetImg('X'); 
+            resetImg('X');
 		}
 		else if (conditionCircle[a] == true)
 		{
 			ply2++;
 			tempName = a;
-			flag = 1;
-			document.querySelector('#player_1_name').style.cssText = 'border-bottom-style: none;';
-			document.querySelector('#player_2_name').style.cssText = 'border-bottom-style: none;';
-			userIndicate(1);
-			resetImg('O');
+            resetImg('O');
 		}
-	}
-	
-	
+    }
 }
 
 function resetImg(abc)
@@ -115,6 +109,8 @@ function resetGame() {
 		document.querySelector(`#${arrEliments[m]}`).setAttribute('class', `${arrDefaultAttributeName[m]}`);
 	}
 
+	document.querySelector('#player_1').setAttribute('class', 'score');
+	document.querySelector('#player_2').setAttribute('class', 'score');
 	document.querySelector(`#${arrConditions[tempName][0]}`).setAttribute('class', `${document.querySelector(`#${arrConditions[tempName][0]}`).getAttribute('class')}`);
 	document.querySelector(`#${arrConditions[tempName][1]}`).setAttribute('class', `${document.querySelector(`#${arrConditions[tempName][1]}`).getAttribute('class')}`);
 	document.querySelector(`#${arrConditions[tempName][2]}`).setAttribute('class', `${document.querySelector(`#${arrConditions[tempName][2]}`).getAttribute('class')}`);
@@ -134,4 +130,3 @@ function resetGame() {
 		document.querySelector(`#${arrEliments[i]}`).setAttribute('onclick', 'addImg(this.id)');
 	}
 }
-
